ac3a96f98a32fd5cb9ac12287e07b5a2f67cd50d
Ganagani Renuka <ganaganirenuka@gmail.com>
2026-07-06 10:20:44 +0530

    temp commit before rewrite

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class LoanInput(BaseModel):
+    age: int
+    income: int
+    loan_amount: int
+
 @app.get("/")
 def home():
-    return {"message": "Loan Default Prediction API is running"}
+    return {"message": "API is running"}
+
+@app.post("/predict")
+def predict(data: LoanInput):
+    return {
+        "received_data": data,
+        "result": "working"
+    }
