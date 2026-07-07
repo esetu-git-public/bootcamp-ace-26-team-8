bb2fb22c6ee4782f5512fd8be6b29b415c51fc47
Ganagani Renuka <ganaganirenuka@gmail.com>
2026-07-07 10:19:26 +0530

    Recreated backend structure properly

--- backend/main.py
+++ backend/main.py
@@ -1,20 +1,7 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
 
 app = FastAPI()
 
-class LoanInput(BaseModel):
-age: int
-income: int
-loan_amount: int
-
 @app.get("/")
 def home():
-return {"message": "Loan Default Prediction API is running"}
-
-@app.post("/predict")
-def predict(data: LoanInput):
-return {
-"received_data": data,
-"result": "working"
-}
+    return {"message": "Loan Default Prediction API is running"}
